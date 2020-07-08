8b450fff0305c13b84ac3ccaef10826e292649e1
Damien <damien@dam.io>
2020-07-08 14:34:09 +0200

    ajoute un rendu #MVP

--- rendu_html.py
+++ rendu_html.py
@@ -4,7 +4,9 @@ categories = json.load(open('commerces.json'))
 
 print('<pre>')
 for categorie, commerces in categories.items():
-	print('##', categorie)
+	print('<h1>', categorie, '</h1>', sep='')
 	for commerce in commerces:
 		for attribut, valeur in commerce.items():
 			print(attribut, ':', valeur)
+		print()
+	print()
